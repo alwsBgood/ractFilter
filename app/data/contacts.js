59c4d160d836e6f6a4094cc23302019b6633e0ec
Dmitriy <dima.chebotaru@gmail.com>
2017-02-06 19:19:38 +0200

    lesson 10

---
+++ app/data/contacts.js
@@ -0,0 +1,25 @@
+let CONTACTS = [
+  {
+    id: 1,
+    name: 'Igor Starodub',
+    phoneNumber: '+250966666666',
+    image: 'app/img/user-1.png'
+  }, {
+    id: 2,
+    name: 'Slava Moseichuk',
+    phoneNumber: '+250966344466',
+    image: 'app/img/user-5.png'
+  }, {
+    id: 3,
+    name: 'Maxim Kusak',
+    phoneNumber: '+250976654433',
+    image: 'app/img/user-7.png'
+  }, {
+    id: 4,
+    name: 'Alexandr Kovalskiy',
+    phoneNumber: '+250456784935',
+    image: 'app/img/user-2.png'
+  }
+]
+
+export default CONTACTS
